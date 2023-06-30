e2e14263779e131625deedf9d229e90ab62b15dd
Kyle Swovick <99501054+kswovick@users.noreply.github.com>
2023-06-30 09:19:59 -0400

    Update create_metadata.R

--- R/create_metadata.R
+++ R/create_metadata.R
@@ -7,22 +7,18 @@
 #' @return A matrix that has a condition name for every sample name
 #' @export
 #'
-create_metadata <- function(
-      data = data
-      ) {
+create_metadata <- function(data = data) {
 
    #Pull out just the sample names from data
    sample_names <- data %>%
       dplyr::distinct(sample)
    conditions <- readline('What is the condition of each sample in order? (ensure to seaprate each condition by a comma)')
-   conditions <- strsplit(conditions, ',')
+   conditions <- strsplit(conditions, 
+                          ',')
 
-   sample_annotation <- data.frame(
-      sample_names,
-      conditions
-   ) %>%
-      purrr::set_names(
-         'sample',
-         'condition'
-      )
+   sample_annotation <- data.frame(sample_names,
+                                   conditions) %>%
+      purrr::set_names('sample',
+                       'condition')
+      
 }
