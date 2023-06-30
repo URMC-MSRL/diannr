7ab20bf32fb8a2838adde50954de946fd46aadb3
Kyle Swovick <99501054+kswovick@users.noreply.github.com>
2023-06-30 09:10:41 -0400

    Update annotate_peptide.R

--- R/annotate_peptide.R
+++ R/annotate_peptide.R
@@ -1,16 +1,9 @@
 pep_count_fun <- function(df) {
-   as.data.frame(
-      table(
-         unique(
-            df[,
-               c(
-                  'protein_group',
-                  'modified_peptide'
-               )
-            ]
-         )$'protein_group'
-      )
-   )
+   
+   as.data.frame(table(unique(df[,
+                                 c('protein_group',
+                                   'modified_peptide')])$'protein_group'))
+   
 }
 
 #' Combine Metadata with Peptide-level Data
@@ -24,55 +17,38 @@ pep_count_fun <- function(df) {
 #' @return A data frame containing peptide-level MaxLFQ intensities and peptide count for each protein group
 #' @export
 #'
-annotate_peptide <- function(
-      data = data,
-      sample_annotation = sample_annotation) {
+annotate_peptide <- function(data = data,
+                             sample_annotation = sample_annotation) {
 
    pep_data_filtered <- data %>%
-      dplyr::left_join(
-         y = sample_annotation,
-         by = 'sample'
-      ) %>%
-      dplyr::relocate(
-         condition,
-         .after = sample
-      )
+      dplyr::left_join(y = sample_annotation,
+                       by = 'sample') %>%
+      dplyr::relocate(condition,
+                      .after = sample)
 
    #Count the number of peptides quantified (per protein group) in each samples
    pep_count <- data %>%
-      dplyr::select(
-         c(
-            'sample',
-            'protein_group',
-            'modified_peptide'
-         )
-      ) %>%
+      dplyr::select(c('sample',
+                      'protein_group',
+                      'modified_peptide')) %>%
       dplyr::group_by(sample) %>%
       tidyr::nest() %>%
-      dplyr::mutate(
-         n_peptides = purrr::map(data, pep_count_fun)
-      ) %>%
-      dplyr::select(
-         -c('data')
-      ) %>%
+      dplyr::mutate(n_peptides = purrr::map(data, pep_count_fun)) %>%
+      dplyr::select(-c('data')) %>%
       tidyr::unnest(n_peptides)  %>%
-      purrr::set_names(
-         'sample',
-         'protein_group',
-         'number_precursors'
-      )
+      purrr::set_names('sample',
+                       'protein_group',
+                       'number_precursors')
 
-   pep_count$sample <- sub("^[^_]*_([^_]*).*", "\\1", pep_count$sample)
+   pep_count$sample <- sub("^[^_]*_([^_]*).*", 
+                           "\\1", 
+                           pep_count$sample)
 
    peptide_data <- pep_data_filtered %>%
-      dplyr::left_join(
-         y = pep_count,
-         by = c('sample', 'protein_group')
-      ) %>%
-      dplyr::relocate(
-         number_precursors,
-         .after = peak_width
-      )
+      dplyr::left_join(y = pep_count,
+                       by = c('sample', 'protein_group')) %>%
+      dplyr::relocate(number_precursors,
+                      .after = peak_width)
 
    return(peptide_data)
 }
